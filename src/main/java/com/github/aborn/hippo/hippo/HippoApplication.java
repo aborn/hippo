4304b18ed8affb4472c7a81bb308d8b64f8f75fc
Ru Chuan <aborn@MacBook-Pro.local>
2020-05-31 08:02:24 +0800

    init

---
+++ src/main/java/com/github/aborn/hippo/hippo/HippoApplication.java
@@ -0,0 +1,13 @@
+package com.github.aborn.hippo.hippo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HippoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HippoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/aborn/hippo/hippo/HippoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aborn.hippo.hippo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HippoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
