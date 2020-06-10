3febe061d2748fed96cb6a58ccedaf0d0b240cdc
aborn <loveaborn@foxmail.com>
2020-06-10 23:53:05 +0800

    main

--- hippo-sdk/src/main/java/com/github/aborn/hippo/hipposdk/HippoClient.java
+++ hippo-sdk/src/main/java/com/github/aborn/hippo/hipposdk/HippoClient.java
@@ -1,4 +1,8 @@
 package com.github.aborn.hippo.hipposdk;
 
 public class HippoClient {
+
+    public static void main(String[] args) {
+        System.out.println("our current:" + System.currentTimeMillis());
+    }
 }
